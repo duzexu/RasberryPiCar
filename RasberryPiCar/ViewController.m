44afcd35e69f612f9db20a98b4fd5403abdc3d13
duzexu <zexu007@qq.com>
2017-05-02 00:11:09 +0800

    bug fix

--- RasberryPiCar/ViewController.m
+++ RasberryPiCar/ViewController.m
@@ -9,12 +9,14 @@
 #import "ViewController.h"
 #import "ADSocketLoger.h"
 #import "ADRocker.h"
-#import <IJKMediaFramework/IJKMediaFramework.h>
+//#import <IJKMediaFramework/IJKMediaFramework.h>
 
-@interface ViewController ()<ADRockerDelegate> {
+@interface ViewController ()<ADRockerDelegate,UIGestureRecognizerDelegate> {
     CGPoint _point;
     BOOL _isTurning;
 }
+
+@property (strong, nonatomic) IBOutlet ADRocker *rockerView;
 
 @end
 
@@ -23,12 +25,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [[ADSocketLoger sharedLogger] connectWithHost:@"192.168.1.110" port:@"8080"];
-    [IJKFFMoviePlayerController setLogLevel:k_IJK_LOG_VERBOSE];
-    IJKFFMoviePlayerController *player = [[IJKFFMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:@"http://192.168.1.110:8090"] withOptions:nil];
-    player.view.frame = [UIScreen mainScreen].bounds;
-    [self.view insertSubview:player.view atIndex:0];
-    [player play];
-    [self installMovieNotificationObservers];
 }
 
 - (IBAction)swipeAction:(UIPanGestureRecognizer *)sender {
@@ -71,114 +67,10 @@
     [[ADSocketLoger sharedLogger] sendCommandData:json];
 }
 
-#pragma Selector func
-
-- (void)loadStateDidChange:(NSNotification*)notification {
-//    IJKMPMovieLoadState loadState = _player.loadState;
-//    
-//    if ((loadState & IJKMPMovieLoadStatePlaythroughOK) != 0) {
-//        NSLog(@"LoadStateDidChange: IJKMovieLoadStatePlayThroughOK: %d\n",(int)loadState);
-//    }else if ((loadState & IJKMPMovieLoadStateStalled) != 0) {
-//        NSLog(@"loadStateDidChange: IJKMPMovieLoadStateStalled: %d\n", (int)loadState);
-//    } else {
-//        NSLog(@"loadStateDidChange: ???: %d\n", (int)loadState);
-//    }
-}
-
-- (void)moviePlayBackFinish:(NSNotification*)notification {
-    int reason =[[[notification userInfo] valueForKey:IJKMPMoviePlayerPlaybackDidFinishReasonUserInfoKey] intValue];
-    switch (reason) {
-        case IJKMPMovieFinishReasonPlaybackEnded:
-            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonPlaybackEnded: %d\n", reason);
-            break;
-            
-        case IJKMPMovieFinishReasonUserExited:
-            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonUserExited: %d\n", reason);
-            break;
-            
-        case IJKMPMovieFinishReasonPlaybackError:
-            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonPlaybackError: %d\n", reason);
-            break;
-            
-        default:
-            NSLog(@"playbackPlayBackDidFinish: ???: %d\n", reason);
-            break;
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+    if ([touch.view isDescendantOfView:_rockerView]) {
+        return NO;
     }
+    return YES;
 }
-
-- (void)mediaIsPreparedToPlayDidChange:(NSNotification*)notification {
-    NSLog(@"mediaIsPrepareToPlayDidChange\n");
-}
-
-- (void)moviePlayBackStateDidChange:(NSNotification*)notification {
-//    switch (_player.playbackState) {
-//        case IJKMPMoviePlaybackStateStopped:
-//            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: stoped", (int)_player.playbackState);
-//            break;
-//            
-//        case IJKMPMoviePlaybackStatePlaying:
-//            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: playing", (int)_player.playbackState);
-//            break;
-//            
-//        case IJKMPMoviePlaybackStatePaused:
-//            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: paused", (int)_player.playbackState);
-//            break;
-//            
-//        case IJKMPMoviePlaybackStateInterrupted:
-//            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: interrupted", (int)_player.playbackState);
-//            break;
-//            
-//        case IJKMPMoviePlaybackStateSeekingForward:
-//        case IJKMPMoviePlaybackStateSeekingBackward: {
-//            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: seeking", (int)_player.playbackState);
-//            break;
-//        }
-//            
-//        default: {
-//            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: unknown", (int)_player.playbackState);
-//            break;
-//        }
-//    }
-}
-
-#pragma Install Notifiacation
-
-- (void)installMovieNotificationObservers {
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(loadStateDidChange:)
-                                                 name:IJKMPMoviePlayerLoadStateDidChangeNotification
-                                               object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(moviePlayBackFinish:)
-                                                 name:IJKMPMoviePlayerPlaybackDidFinishNotification
-                                               object:nil];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(mediaIsPreparedToPlayDidChange:)
-                                                 name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification
-                                               object:nil];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(moviePlayBackStateDidChange:)
-                                                 name:IJKMPMoviePlayerPlaybackStateDidChangeNotification
-                                               object:nil];
-    
-}
-
-- (void)removeMovieNotificationObservers {
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:IJKMPMoviePlayerLoadStateDidChangeNotification
-                                                  object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:IJKMPMoviePlayerPlaybackDidFinishNotification
-                                                  object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification
-                                                  object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:IJKMPMoviePlayerPlaybackStateDidChangeNotification
-                                                  object:nil];
-    
-}
-
 @end
